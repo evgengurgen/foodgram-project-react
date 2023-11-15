f45580107a60d18f00f0fd5b44ad718fb188730b
Евгений Волобуев <afondurin@yandex.ru>
2023-11-16 02:15:11 +0300

    Fix filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,7 +5,22 @@ from recipes.models import Recipe
 class RecipeFilter(filters.FilterSet):
     tags = filters.CharFilter(field_name='tags__slug',
                               lookup_expr='iexact')
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ['author', 'name', 'tags']
+        fields = ('author', 'name', 'tags',
+                  'is_favorited', 'is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value:
+            return queryset.filter(is_favorited=True)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return queryset.filter(is_in_shopping_cart=True)
+        return queryset
